024bb7cbf1864e3be8fab4abc8b6398021b7d2a1
MIRLAZIZ <mirlazizmirtolipov6@gmail.com>
2022-02-17 13:01:23 +0500

    ozgarishlar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+  
+
+
+    
      <router-view/>
   </div>
  
@@ -16,17 +18,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-      background: #f1f1f1;
-    }
-  }
-}
 </style>
